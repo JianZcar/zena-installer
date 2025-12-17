222634d5dadd492f11a2d9f948b2661f4fc1a808
JianZcar <esteban.jianzcar@outlook.com>
2025-12-18 04:51:51 +0800

    make sure that the install logs shows up visually

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ skopeo copy \
     --preserve-digests \
     "oci:/etc/zena:stable" \
     "containers-storage:ghcr.io/jianzcar/zena:stable"
-echo "Image imported. Switching Bootc to use the local image..."
+echo "Installing Arch(zena) please wait..."
 /usr/bin/bootc switch --transport containers-storage "ghcr.io/jianzcar/zena:stable" --apply
 EOF
 chmod +x /usr/libexec/install-zena.sh
@@ -19,16 +19,19 @@ chmod +x /usr/libexec/install-zena.sh
 cat << 'EOF' > /etc/systemd/system/install-zena.service
 [Unit]
 Description=Zena installer
-After=local-fs.target sysinit.target
+After=local-fs.target sysinit.target getty@tty1.service
+Before=getty@tty1.service
 Requires=local-fs.target
 RequiresMountsFor=/etc/zena
 
 [Service]
 Type=oneshot
+ExecStartPre=-/bin/systemctl stop getty@tty1.service
 ExecStart=/usr/libexec/install-zena.sh
-StandardOutput=journal+console
-StandardError=journal+console
-TTYPath=/dev/console
+ExecStartPost=/bin/systemctl start getty@tty1.service
+StandardOutput=tty
+StandardError=tty
+TTYPath=/dev/tty1
 TTYReset=yes
 RemainAfterExit=yes
 
