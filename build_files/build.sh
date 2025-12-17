d46991690d24ecbcb98b632b07fc4cd7d1cc3d58
JianZcar <esteban.jianzcar@outlook.com>
2025-12-17 15:38:27 +0800

    install core packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -50,7 +50,7 @@ RemainAfterExit=yes
 WantedBy=multi-user.target
 EOF
 
-dnf -y groupinstall "Core" "Minimal Install" \
-    --setopt=install_weak_deps=False \
-    --nodocs
+
+dnf5 -y group install "Core"
+dnf5 -y group install "Minimal Install"
 systemctl enable install-zena.service
